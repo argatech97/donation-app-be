2f494fce8783a6dc9ac8148808c89fd6b8bfcd74
argatech97 <argatech97@gmail.com>
2024-02-18 11:45:21 +0700

    modify campaign module

--- src/module/campaign/entity/RequestPayload.entity.ts
+++ src/module/campaign/entity/RequestPayload.entity.ts
@@ -2,9 +2,3 @@ import { IData } from "@module/db";
 import { ICampaign } from "./Campaign.entity";
 
 export interface ICampaignRequestPayload extends Omit<ICampaign, "id">, IData {}
-
-export interface ICreateCampaignPayload {
-  campaign: Omit<ICampaign, "id" | "storyId">;
-  thumbnail: File;
-  story: string;
-}
